d722aa7d34255f40956fe3a190a60305604d3cb9
Andrew Nikonchuk <anikonch@gmail.com>
2018-07-16 14:15:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Student
   attr_accessor :id, :name, :grade
 
@@ -99,6 +101,7 @@ class Student
 
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
+      binding.pry
     end
   end
 
